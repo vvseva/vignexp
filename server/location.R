6d26a7d2edab54471ba2f2e8de2433fe9a982562
Vsevolod Suschevskiy <50490206+vvseva@users.noreply.github.com>
2021-10-16 01:18:35 +0300

    Add files via upload

--- server/location.R
+++ server/location.R
@@ -1,4 +1,4 @@
-
+load("map.RData")
 observeEvent(input$btn_ask_geo1, {
   # Run JS code that simply shows a message
   runjs('  $(document).ready(function () {
@@ -35,7 +35,7 @@ output$geo_collect <- renderUI({
     </p>"),
     # Set up shinyjs
     fluidRow(
-      column(12, align="center",
+      column(10, align="center",
         actionButton("btn_ask_geo1", "Share location", style="background-color: #EBEBEC"),
         actionButton("btn_ask_geo0", "No")
       )
@@ -45,16 +45,47 @@ output$geo_collect <- renderUI({
 
 
 output$geo_show_text <- renderUI({
+  
+  if (is.null(input$lat)) {
+    tagList(HTML("
+                 no location, please select manually
+                 "))
+    
+    fluidRow(
+      column(10, align="center",
+             select2Input("select2Input1",
+               "This is a multiple select2Input",
+                choices=c("a","b","c"),
+                selected=c("b","a"),
+                width = "600px"
+                )
+      )
+    )
+  } else {
+  geo_state = str_glue(
+    "http://open.mapquestapi.com/nominatim/v1/reverse.php?key={key_}&format=json&lat={lat_}&lon={long_}",
+    key_ = OSM_API_KEY,
+    lat_ = input$lat,
+    long_ = input$long) |> 
+    GET() |>
+    httr::content() |>
+    purrr::pluck("address") |>
+    purrr::pluck("state")
+  
   tagList(HTML(
     str_c(
       "<p>",
       "Your location is ",
       "lattitude:", input$lat,
+      "<br>",
       "longitude:", input$long,
       "<br>",
-      "geolocation:", input$geolocation,
+      "geolocation:", 
+      geo_state,
       "</p>",
       sep = " "
     )
   ))
+}
+  
 })
