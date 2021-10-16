dd12db9afc83bf881b80492dfbd1c1a98ca3056a
Vsevolod Suschevskiy <50490206+vvseva@users.noreply.github.com>
2021-10-16 17:44:45 +0300

    Add files via upload

--- server/generate_1.R
+++ server/generate_1.R
@@ -3,48 +3,146 @@ random_df <- tibble(
   social = c("weak", "strong", "no_social"),
   geo = c("geo", "no_geo", "no_geo")
 ) |> tidyr::expand(motivation, social, geo) |>
-  slice_sample(prop = 1)
+  slice_sample(prop = 1) |> 
+  right_join(table_texts) |> 
+  mutate(text  = replace_na(text, "I did not come up with a text"))
 
 
-# vign_1 <- reactiveValues(values = random_df |> head(1))
 
+generate_vignette <- function(i = 1, city_location = rtc_geo_state()){
+  tagList(HTML(
+    str_c(
+      "<p>", 
+      str_glue(random_df$text[i], city_name = city_location),
+      "</p>",
+      sep = " "
+    )
+  ),
+  sliderInput(inputId = paste0("sliderInput_", i), 
+              label = div(style='width:400px;', 
+                          div(style='float:left;', 'more likely'), 
+                          div(style='float:right;', 'less likely')),  
+              min = 0, max = 100, value = 1,
+              width = 400
+  ))
+}
 
 output$vign_1_text <- renderUI({
-  tagList(HTML(
-    str_c(
-      "<p>", 
-      "motivation:", random_df$motivation[1],
-      "social:", random_df$social[1],
-      "geo:",random_df$geo[1],
-      "</p>",
-      sep = " "
-    )
-  ),
-  sliderInput(inputId = "n", 
-              label = div(style='width:400px;', 
-                          div(style='float:left;', 'more likely'), 
-                          div(style='float:right;', 'less likely')),  
-              min = 0, max = 100, value = 1,
-              width = 400
-  ))
-})
+    generate_vignette(i = 1, 
+    city_location = rtc_geo_state())
+  })
 
 output$vign_2_text <- renderUI({
-  tagList(HTML(
-    str_c(
-      "<p>", 
-      "motivation:", random_df$motivation[2],
-      "social:", random_df$social[2],
-      "geo:",random_df$geo[2],
-      "</p>",
-      sep = " "
-    )
-  ),
-  sliderInput(inputId = "n", 
-              label = div(style='width:400px;', 
-                          div(style='float:left;', 'more likely'), 
-                          div(style='float:right;', 'less likely')),  
-              min = 0, max = 100, value = 1,
-              width = 400
-  ))
+  generate_vignette(i = 2, 
+                    city_location = rtc_geo_state())
 })
+
+output$vign_3_text <- renderUI({
+  generate_vignette(i = 3, 
+                    city_location = rtc_geo_state())
+})
+
+output$vign_4_text <- renderUI({
+  generate_vignette(i = 4, 
+                    city_location = rtc_geo_state())
+})
+
+output$vign_5_text <- renderUI({
+  generate_vignette(i = 5, 
+                    city_location = rtc_geo_state())
+})
+
+output$vign_6_text <- renderUI({
+  generate_vignette(i = 6, 
+                    city_location = rtc_geo_state())
+})
+
+output$vign_7_text <- renderUI({
+  generate_vignette(i = 7, 
+                    city_location = rtc_geo_state())
+})
+
+output$vign_8_text <- renderUI({
+  generate_vignette(i = 8, 
+                    city_location = rtc_geo_state())
+})
+
+output$vign_9_text <- renderUI({
+  generate_vignette(i = 9, 
+                    city_location = rtc_geo_state())
+})
+
+output$vign_10_text <- renderUI({
+  generate_vignette(i = 10, 
+                    city_location = rtc_geo_state())
+})
+
+output$vign_11_text <- renderUI({
+  generate_vignette(i = 11, 
+                    city_location = rtc_geo_state())
+})
+output$vign_12_text <- renderUI({
+  generate_vignette(i = 12, 
+                    city_location = rtc_geo_state())
+})
+output$vign_13_text <- renderUI({
+  generate_vignette(i = 13, 
+                    city_location = rtc_geo_state())
+})
+output$vign_14_text <- renderUI({
+  generate_vignette(i = 14, 
+                    city_location = rtc_geo_state())
+})
+output$vign_15_text <- renderUI({
+  generate_vignette(i = 15, 
+                    city_location = rtc_geo_state())
+})
+output$vign_16_text <- renderUI({
+  generate_vignette(i = 16, 
+                    city_location = rtc_geo_state())
+})
+output$vign_17_text <- renderUI({
+  generate_vignette(i = 17, 
+                    city_location = rtc_geo_state())
+})
+output$vign_18_text <- renderUI({
+  generate_vignette(i = 18, 
+                    city_location = rtc_geo_state())
+})
+
+
+# output$vign_1_text <- renderUI({
+#   tagList(HTML(
+#     str_c(
+#       "<p>", 
+#       str_glue(random_df$text[1], city_name = rtc_geo_state()),
+#       "</p>",
+#       sep = " "
+#     )
+#   ),
+#   sliderInput(inputId = "sliderInput_1", 
+#               label = div(style='width:400px;', 
+#                           div(style='float:left;', 'more likely'), 
+#                           div(style='float:right;', 'less likely')),  
+#               min = 0, max = 100, value = 1,
+#               width = 400
+#   ))
+# })
+# 
+# output$vign_2_text <- renderUI({
+#   tagList(HTML(
+#     str_c(
+#       "<p>", 
+#       str_glue(random_df$text[2], city_name = rtc_geo_state()),
+#       "</p>",
+#       sep = " "
+#     )
+#   ),
+#   sliderInput(inputId = "sliderInput_2", 
+#               label = div(style='width:400px;', 
+#                           div(style='float:left;', 'more likely'), 
+#                           div(style='float:right;', 'less likely')),  
+#               min = 0, max = 100, value = 1,
+#               width = 400
+#   ))
+# })
--- server/location.R
+++ server/location.R
@@ -1,4 +1,10 @@
-load("map.RData")
+# shinyjs::hide("selectInput_geo_manual_row")
+# 
+# observeEvent(input$btn_ask_geo0, {
+#   req(input$btn_ask_geo0)
+#   shinyjs::show("selectInput_geo_manual_row")
+# })
+
 observeEvent(input$btn_ask_geo1, {
   # Run JS code that simply shows a message
   runjs('  $(document).ready(function () {
@@ -43,35 +49,80 @@ output$geo_collect <- renderUI({
   )
 })
 
-
-output$geo_show_text <- renderUI({
-  
-  if (is.null(input$lat)) {
-    tagList(HTML("
-                 no location, please select manually
-                 "))
-    
+output$geo_collect_manual <- renderUI({
+  if (input$btn_ask_geo0) {
     fluidRow(
-      column(10, align="center",
-             select2Input("select2Input1",
-               "This is a multiple select2Input",
-                choices=c("a","b","c"),
-                selected=c("b","a"),
-                width = "600px"
-                )
+      column(10, align = "center",
+             selectInput(
+               inputId = "selectInput_geo_manual",
+               label = "Select one of the cities",
+               choices = c("San Francisco",
+                           "New York",
+                           "Chicago",
+                           "Other")
+             )
       )
     )
   } else {
-  geo_state = str_glue(
-    "http://open.mapquestapi.com/nominatim/v1/reverse.php?key={key_}&format=json&lat={lat_}&lon={long_}",
-    key_ = OSM_API_KEY,
-    lat_ = input$lat,
-    long_ = input$long) |> 
-    GET() |>
-    httr::content() |>
-    purrr::pluck("address") |>
-    purrr::pluck("state")
-  
+    HTML("<p>
+       you location will not be stored
+    </p>")
+  }
+})
+
+# toListen_geo <- reactive({
+#   list(input$lat,input$selectInput_geo_manual)
+# })
+
+rtc_geo_state <- reactiveVal("your city")
+
+observeEvent(
+  input$lat, {
+  if (!is.null(input$lat)) {
+    geo_state <- str_glue(
+      "http://open.mapquestapi.com/nominatim/v1/reverse.php?key={key_}&format=json&lat={lat_}&lon={long_}",
+      key_ = OSM_API_KEY,
+      lat_ = input$lat,
+      long_ = input$long) |> 
+      GET() |>
+      httr::content() |>
+      purrr::pluck("address") |>
+      purrr::pluck("state")
+    
+    rtc_geo_state(geo_state)
+  } else {
+    rtc_geo_state("your city")
+  }
+})
+
+observeEvent(
+  input$selectInput_geo_manual,{
+    if (input$selectInput_geo_manual == "Other") {
+      rtc_geo_state("your city")
+    } else {
+      rtc_geo_state(input$selectInput_geo_manual)
+    }
+})
+
+output$geo_show_text <- renderUI({
+  if (is.null(input$lat)) {
+    
+    fluidRow(column(
+      10,
+      align = "center",
+    tagList(HTML("
+                <p>
+                 no location, please select manually
+                 </p>
+                 ")),
+    tagList(HTML(str_glue("
+                <p>
+                 selected city: {location}
+                 </p>
+                 ", location = rtc_geo_state()
+                          )))
+    ))
+  } else {
   tagList(HTML(
     str_c(
       "<p>",
@@ -81,7 +132,8 @@ output$geo_show_text <- renderUI({
       "longitude:", input$long,
       "<br>",
       "geolocation:", 
-      geo_state,
+      # geo_state,
+      rtc_geo_state(),
       "</p>",
       sep = " "
     )
