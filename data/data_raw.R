742bfe6dc850e36359d31b95f8bf40589019f593
Vsevolod Suschevskiy <50490206+vvseva@users.noreply.github.com>
2021-10-16 17:43:58 +0300

    Add files via upload

---
+++ data/data_raw.R
@@ -0,0 +1,3 @@
+# library(googlesheets4)
+# table_texts = read_sheet("https://docs.google.com/spreadsheets/d/1KaJos8X_mV-atpsZMs9-_5-OkM6JltnZgjac-CWVcdw/edit?usp=sharing")
+# save(table_texts, file = "data/texts.RData")
