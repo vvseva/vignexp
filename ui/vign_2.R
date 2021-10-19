054a548a70ddfe0369e5d74fa5ab91a70c2269c6
Vsevolod Suschevskiy <50490206+vvseva@users.noreply.github.com>
2021-10-19 22:57:52 +0300

    Update vign_2.R

--- ui/vign_2.R
+++ ui/vign_2.R
@@ -1,9 +1,9 @@
 tags$section(
   class = "content_page",
-  HTML(
-    "<p>
-        Situation_2
-    </p>"
-  ),
+#   HTML(
+#     "<p>
+#         Situation_2
+#     </p>"
+#   ),
   uiOutput("vign_2_text")
 )
